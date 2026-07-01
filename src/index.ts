d6cf8c2fb0e7bc2168f29797734d40cfcfd26ea9
yueert1997ai-sys <yueert1997ai@gmail.com>
2026-07-01 18:18:49 +0800

    feat: export council module API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { createHanakoCouncilExecutor } from "./hanako-adapter.js";
+export type { HanakoWorkflowHostApi } from "./hanako-adapter.js";
+export { runCouncilMeeting, CouncilMeetingError } from "./runner.js";
+export { normalizeCouncilConfig } from "./validation.js";
+export * from "./types.js";
